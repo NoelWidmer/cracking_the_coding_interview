a8db3e83e7859057e7d3c29b6922d3b1ca396540
Noël Widmer <noel.widmer@gmail.com>
2019-12-07 17:50:25 +0100

    improved solution 1

--- src/solutions/s1.rs
+++ src/solutions/s1.rs
@@ -1,15 +1,15 @@
-use crate::algorithms::merge_sort;
-
-fn are_all_unique<T: Ord>(items: Vec<T>) -> bool {
-    let sorted_items = merge_sort(&items[..]);
+fn are_all_unique<T: Ord + Clone>(items: &mut [T]) -> bool {
+    items.sort_unstable();
 
     let mut prev: Option<&T> = None;
 
-    for item in sorted_items {
-        if prev == Some(item) {
-            return false;
+    for item in items {
+        if let Some(prev) = prev {
+            if prev == item {
+                return false;
+            }
         }
-
+        
         prev = Some(item);
     }
 
@@ -19,44 +19,56 @@ fn are_all_unique<T: Ord>(items: Vec<T>) -> bool {
 #[cfg(test)]
 mod tests {
     #[test]
-    fn no_item_is_unique() {
-        let sut: Vec<usize> = Vec::new();
-        let result = super::are_all_unique(sut);
-        assert!(result);
+    fn given_empty_when_checking_for_uniqueness_then_are_unique() {
+        let sut: Vec<i32> = Vec::new();
+        let res = when_checking_uniqueness(sut);
+        then_are_unique(res);
     }
 
     #[test]
-    fn single_item_is_unique() {
+    fn given_single_item_when_checking_for_uniqueness_then_are_unique() {
         let sut = vec![ 177 ];
-        let result = super::are_all_unique(sut);
-        assert!(result);
+        let res = when_checking_uniqueness(sut);
+        then_are_unique(res);
     }
 
     #[test]
-    fn two_items_are_unique() {
+    fn given_two_unique_items_when_checking_for_uniqueness_then_are_unique() {
         let sut = vec![ 177, 666 ];
-        let result = super::are_all_unique(sut);
-        assert!(result);
+        let res = when_checking_uniqueness(sut);
+        then_are_unique(res);
     }
 
     #[test]
-    fn two_items_are_not_unique() {
-        let sut = vec![ 177, 117 ];
-        let result = super::are_all_unique(sut);
-        assert!(result);
+    fn given_two_non_unique_items_when_checking_for_uniqueness_then_are_not_unique() {
+        let sut = vec![ 117, 117 ];
+        let res = when_checking_uniqueness(sut);
+        then_are_not_unique(res);
     }
 
     #[test]
-    fn multiple_items_are_unique() {
+    fn given_multiple_unqiue_items_when_checking_for_uniqueness_then_are_unique() {
         let sut = vec![ 177, 666, 22, 99, 11, 1, 33, 0 ];
-        let result = super::are_all_unique(sut);
-        assert!(result);
+        let res = when_checking_uniqueness(sut);
+        then_are_unique(res);
     }
 
     #[test]
-    fn multiple_items_are_not_unique() {
-        let sut = vec![ 177, 666, 22, 99, 11, 117, 33, 0 ];
-        let result = super::are_all_unique(sut);
-        assert!(result);
+    fn given_multiple_non_unqiue_items_when_checking_for_uniqueness_then_are_not_unique() {
+        let sut = vec![ 177, 666, 22, 99, 11, 117, 33, 22, 0 ];
+        let res = when_checking_uniqueness(sut);
+        then_are_not_unique(res);
+    }
+
+    fn when_checking_uniqueness(mut sut: Vec<i32>) -> bool {
+        super::are_all_unique(&mut sut)
+    }
+
+    fn then_are_unique(res: bool) {
+        assert!(res);
+    }
+
+    fn then_are_not_unique(res: bool) {
+        assert!(!res);
     }
 }
