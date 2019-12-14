c89f967d63369ebf1e00d5ce987d925e2e781905
Noël Widmer <noel.widmer@gmail.com>
2019-12-14 13:42:35 +0100

    arranged the solutions into chapters

---
+++ src/solutions/chapter1/mod.rs
@@ -0,0 +1,8 @@
+mod s1;
+mod s2;
+mod s3;
+mod s4;
+mod s5;
+mod s6;
+mod s8;
+mod s9;
--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -1,8 +1,2 @@
-mod s1;
-mod s2;
-mod s3;
-mod s4;
-mod s5;
-mod s6;
-mod s8;
-mod s9;
+mod chapter1;
+mod chapter2;
